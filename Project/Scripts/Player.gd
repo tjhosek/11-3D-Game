d30f53cb1cdc30999f91d2cf817aa12b858ebf80
Trevor Hosek <trevorhosek@gmail.com>
2019-11-21 18:38:39 -0500

    Put in basic elements of an fps

---
+++ Project/Scripts/Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody
+
+var movespeed = 5
+var jumpspeed = 3
+onready var cameraHolder = get_node("CameraHolder")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Project/Scripts/World.gd
@@ -0,0 +1,3 @@
+extends Spatial
+
+# Camera.trauma() adds screen shake
